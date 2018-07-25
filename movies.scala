27a44c9233087aeddf2d64999faca1aefed86f51
Tempy <rtemple@ramapo.edu>
2018-07-25 11:16:07 -0400

    Cleaned up and commented

--- movies.scala
+++ movies.scala
@@ -5,8 +5,14 @@ val token = file.map(x=>x.split(","))
 val keyval = token.map(x=>(x(0),(x(1),x(2))))
 //Reduce
 val maxRating = keyval.reduceByKey{case(a,b)=> if(a._2>b._2){a} else {b}}
+//Display top movie for each user
 maxRating.collect.foreach(println)
-//List the max movies with rating
-val totalMax = maxRating.values.max
-val filter = maxRating.filter{case(a,b)=> b._2 == totalMax._2}
+
+//Create another Key/Value pair
+val movieRatingList = token.map(x=>(x(1), x(2)))
+//Get max rating for new list
+val highestRatedValue = movieRatingList.values.max
+//Filter list for all values w/ highestRatedValue
+val filter = movieRatingList.filter{case(key,value)=> value==highestRatedValue}
+//Display top movies for all total list
 filter.collect.foreach(println)
