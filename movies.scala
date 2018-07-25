dcc96babb1d6c113c76ba1353585a7d5a2d82e01
Tempy <rtemple@ramapo.edu>
2018-07-25 10:59:36 -0400

    Assignment 4 completion

--- movies.scala
+++ movies.scala
@@ -3,4 +3,10 @@ val file = sc.textFile("/home/temp/input/movies.txt")
 //Create Key/Value pair
 val token = file.map(x=>x.split(","))
 val keyval = token.map(x=>(x(0),(x(1),x(2))))
-keyval.collect.foreach(println)
+//Reduce
+val maxRating = keyval.reduceByKey{case(a,b)=> if(a._2>b._2){a} else {b}}
+maxRating.collect.foreach(println)
+//List the max movies with rating
+val totalMax = maxRating.values.max
+val filter = maxRating.filter{case(a,b)=> b._2 == totalMax._2}
+filter.collect.foreach(println)
